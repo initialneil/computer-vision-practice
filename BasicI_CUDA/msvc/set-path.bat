6f8f56f0bc089f9b3bf7acd71463d84778c0dc9e
Neil Z. Shao <initialneil@gmail.com>
2022-04-27 13:39:13 +0800

    update set-path.bat

--- BasicI_CUDA/msvc/set-path.bat
+++ BasicI_CUDA/msvc/set-path.bat
@@ -1,7 +1,9 @@
 REM This is a bat for setting required system variables. Please change specific path to your own.
-set TOOLKITS=D:\toolkits
+set TOOLKITS=C:\toolkits
+setx TOOLKITS %TOOLKITS%
 
 setx OPENCV_3_4_3_CUDA_10_0 "%TOOLKITS%\opencv-3.4.3-contrib-cuda10.0"
+setx OPENCV_3_4_3_CPU "%TOOLKITS%\opencv-3.4.3-prebuilt\build"
 setx BOOST_1_68_0 "%TOOLKITS%\boost_1_68_0"
 setx LIB_PROTOBUF_3_0_0 "%TOOLKITS%\protobuf-3.0.0"
 
@@ -19,9 +21,10 @@ setx ZLIB "%TOOLKITS%\zlib"
 setx LIB_OPENGL "%TOOLKITS%\opengl-utils"
 setx LIB_JPEG "%TOOLKITS%\libjpeg-turbo64"
 setx LIB_JSONCPP "%TOOLKITS%\jsoncpp-2018.10.3"
+setx JSONCPP_1_9_2 "%TOOLKITS%\jsoncpp-1.9.2\vs2015-x64"
 
 setx LIB_STREAM "%TOOLKITS%\stream-utils"
 
-setx libAzureKinectSDK "%TOOLKITS%\Azure\1.4.0-alpha.0"
+setx libAzureKinectSDK "%TOOLKITS%\Azure\1.4.3"
 
 pause
